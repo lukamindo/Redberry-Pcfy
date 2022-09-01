6f2dba1f8385c1d5b8da1690947b99e58ab17fe7
lukamind <luka.mindiashvili14@gmail.com>
2022-09-01 21:10:53 +0400

    add submit page vol.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import FormWrapper from "pages/Form/FormWrapper";
 import Landing from "pages/Landing/Landing";
+import SubmitPage from "pages/Pop-up/SubmitPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/form" element={<FormWrapper />} />
+      <Route path="/submit" element={<SubmitPage />} />
     </Routes>
   );
 }
