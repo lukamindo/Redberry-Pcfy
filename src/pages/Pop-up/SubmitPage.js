ec317a69e483255f7e0d9c272683730f9438eb7a
lukamind <luka.mindiashvili14@gmail.com>
2022-09-01 21:10:37 +0400

    add submit page

---
+++ src/pages/Pop-up/SubmitPage.js
@@ -0,0 +1,47 @@
+import Button from "library/component/button/Button";
+import { useNavigate } from "react-router-dom";
+import {
+  StyledPopUpModal,
+  StyledSubmitPageBackGround,
+} from "./SubmitPage.styled";
+import SubmitIcon from "assets/imgs/SubmitIcon.svg";
+
+function SubmitPage() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledSubmitPageBackGround>
+      <StyledPopUpModal>
+        <img src={SubmitIcon}></img>
+        <p>ჩანაწერი დამატებულია!</p>
+        <Button margin="79px 275px 0 275px" width="297px">
+          სიაში გადაყვანა
+        </Button>
+        <Button
+          onClick={() => navigate("/")}
+          backgroundColor="#ffffff"
+          width="297px"
+          fontColor="#0089A7"
+          margin="0 0  30px 0"
+        >
+          მთავარი
+        </Button>
+      </StyledPopUpModal>
+    </StyledSubmitPageBackGround>
+  );
+}
+
+export default SubmitPage;
+
+{
+  /* <LandingWrapper>
+      <img src={logo} alt="redberry" />
+      <img className="second_img" src={landingimg} alt="PCfy" />
+      <Button onClick={() => navigate("/form")} margin=" 123px 0 0 0 ">
+        ჩანაწერის დამატება
+      </Button>
+      <Button onClick={() => navigate("/form")} margin=" 26px 0 0 0 ">
+        ჩანაწერების სია
+      </Button>
+    </LandingWrapper> */
+}
---
+++ src/pages/Pop-up/SubmitPage.styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const StyledSubmitPageBackGround = styled.div`
+  background: #4a4a4a;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1080px;
+`;
+
+export const StyledPopUpModal = styled.div`
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+
+  img {
+    margin-top: 30px;
+  }
+
+  p {
+    font-weight: 700;
+    font-size: 25px;
+    color: #292929;
+  }
+`;
